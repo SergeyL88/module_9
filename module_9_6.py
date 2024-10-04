c1e29b8b017f2d65ddec38a9ef0395d4b6eba26c
SergeyL88 <Qwerty@3112311>
2024-10-04 22:29:10 +0400

    Генераторы

--- module_9_6.py
+++ module_9_6.py
@@ -1,26 +1,22 @@
+# 1 вариант
 def all_variants(text):
-    for i in range(len(text)):
-        if i == len(text):
-            yield text
-        else:
-            for j in range(len(text)):
-                if i == 0:
-                    yield text[j]
-                if i == j:
-                    continue
-                elif i == j+1:
-                    yield (text[j]+text[i])
-    yield text
+    len_ = len(text)
+    for i in range(1, len_):
+        for j in range(len_):
+            if j + i > len_:
+                yield text
+            else:
+                yield (text[j:i+j])
+
+
+# 2 вариант
+
+# def all_variants(text):
+#     for i in range(len(text)):
+#         for j in range(i + 1, len(text) + 1):
+#             yield text[i:j]
 
 
 a = all_variants("abc")
 for i in a:
     print(i)
-
-
-# a
-# b
-# c
-# ab
-# bc
-# abc
